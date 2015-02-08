aebec9e462eb09f79d3aa5c6d98280a79e911605
SterlingVix <amelocik@gmail.com>
2015-02-07 16:36:25 -0800

    add Location service

--- www/js/location.js
+++ www/js/location.js
@@ -1,55 +1,40 @@
+/**
+ * geolocation.prototype is:
+ *   getCurrentPosition()
+ *   clearWatch()
+ *
+ * Returns:
+ *   coordinates: {
+ *     latitude: 123,
+ *     longitude: 123,
+ *     accuracy: 123 // in meters
+ *   } // end coordinates
+ **/
 var Location = function () {
+    var _this = this;
 
-
-    this.options = {
+    _this.options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
     };
 
-//    {
-//        users: {
-//            "Aaron Melocik": {
-//                "pollsCreated": {
-//
-//                }
-//            },
-//            "Mike Luby": {
-//
-//            }
-//        }
-//
-//
-//    }
-
-
-}
-
-
-
-
-
-
-
-if ("geolocation" in navigator) {
-    /**
-     * geolocation.prototype is:
-     *
-     *   getCurrentPosition()
-     *   clearWatch()
-     **/
-
-
-    var location = navigator.geolocation;
-
-} else {
-    console.log('not here')
-}
-
-
-
-
-
-};
+    _this.coordinates = {};
+}; // end Location()
 
 Location.prototype.constructor = Location;
+
+Location.prototype.getLocation = function() {
+  var _this = this;
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      _this.coordinates = pos.coords;
+    };
+
+    console.log('DEBUG: position is ',  _this.coordinates.latitude, _this.coordinates.longitude, '; accuracy = ', _this.coordinates.accuracy, ' m.'); // Your current position is:
+    return _this.coordinates;
+  }, function(error) { // end if ("geolocation" in navigator)
+    console.warn('ERROR(' + err.code + '): ' + err.message, 'your device may not support location.');
+  }, options);
+}; // end getLocation()
